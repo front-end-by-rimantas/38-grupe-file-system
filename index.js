f7c8f0499ecca79abeae60aa5778253a80b066ce
Rimantas <belauzas@yahoo.com>
2022-06-07 20:14:33 +0300

    File read

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('labas rytas');
+import { file } from './lib/file.js';
+import { skaitomKarves } from './lib/karviuTurgus.js';
+
+const karves = await skaitomKarves();
+console.log(karves);
+
+const readStatus = await file.read('users', 'petras.json');
+console.log(readStatus);
+
+
+// const user = {
+//     name: 'Petras',
+//     age: 99,
+//     isLoggedIn: false,
+// }
+
+// const status = file.create('users', 'petras.json', user);
+// console.log('File status:', status);
