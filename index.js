b635181b6609ca768d64efced96e9b436d7e1be7
Rimantas <belauzas@yahoo.com>
2022-06-08 19:29:36 +0300

    File update and delete methods

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import { file } from './lib/file.js';
 
-// const readStatus = await file.read('userssssss', 'petras.json');
-// console.log(readStatus);
+// IIFE - ifis
+(async () => {
 
+    const readStatus1 = await file.read('users', 'maryte.json');
+    console.log('Read:', readStatus1);
 
-const userMaryte = {
-    name: 'Tevs',
-    age: 87,
-    isMarried: false,
-}
-const createStatus = await file.create('users', 'betkas.json', userMaryte);
-console.log(createStatus);
+    const user = {
+        name: 'maryte',
+        age: 87,
+        isMarried: false,
+    }
+    const createStatus = await file.create('users', 'maryte.json', user);
+    console.log('Create:', createStatus);
+
+    const readStatus2 = await file.read('users', 'maryte.json');
+    console.log('Read:', readStatus2);
+
+    user.isMarried = true;
+    const updateStatus = await file.update('users', 'maryte.json', user);
+    console.log('Update:', updateStatus);
+
+    const readStatus3 = await file.read('users', 'maryte.json');
+    console.log('Read:', readStatus3);
+
+    const deleteStatus = await file.delete('users', 'maryte.json');
+    console.log('Delete:', deleteStatus);
+
+    const readStatus4 = await file.read('users', 'maryte.json');
+    console.log('Read:', readStatus4);
+
+})();
